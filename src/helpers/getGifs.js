7be7082de1932bf2f7c1709b4036de930a94360e
JonathanCabuyales <jonathan.cabuyales@epn.edu.ec>
2021-08-13 17:55:01 -0500

    Cambio en el index

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,7 +1,7 @@
 export const getHttp = async(categoria) =>{
     const api = 'deOgmLVImnG5Fmj3Pjt3cYwdcrTkuZRy';
     const limit = 15;
-    const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI(categoria)}&limit=${ limit }&api_key=${ api }`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(categoria)}&limit=${ limit }&api_key=${ api }`;
     const respuesta = await fetch(url);
     const { data } = await respuesta.json();
     const gifs = data.map( img => {
